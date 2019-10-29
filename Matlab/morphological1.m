8745be6e38ebc13deb60229789030debb8f795c1
Hardik Rana <hardikrana276@gmail.com>
2019-10-29 22:28:47 +0530

    morphological done

---
+++ Matlab/morphological1.m
@@ -0,0 +1,21 @@
+clear all;
+close all;
+clc;
+I1 = imread('tm.jpeg')
+
+SE = strel('square',5);     % generate the structuring element
+figure,imshow(I1),title('Original Image');
+O = imopen(I1,SE);          % Perform Opening Operation
+figure,imshow(O),title('Opened Image');
+
+[I2,map] = imread('closing.png')
+figure,imshow(I2,map),title('Original Image');
+
+%%%find type of image
+handle = image(I2);
+imgmodel = imagemodel(handle); 
+str = getImageType(imgmodel)
+
+I3 = ind2gray(I2,map)
+C = imclose(I3,SE);          % Perform Closing Operation
+figure,imshow(C),title('Closed Image');
