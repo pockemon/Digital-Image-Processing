2d3351f1c9be40f5b9a0ee525f1d2116e19fbd41
Hardik Rana <hardikrana276@gmail.com>
2019-10-31 17:46:46 +0530

    Delete Gaussian.m

--- Matlab/Gaussian.m
+++
@@ -1,3 +0,0 @@
-% Add Gaussian noise to an image
-% https://stackoverflow.com/questions/16008228/using-imnoise-to-add-gaussian-noise-to-an-image
-% https://dsp.stackexchange.com/questions/23830/how-to-add-white-gaussian-noise-to-an-image-in-matlab
